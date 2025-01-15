407ce53290c5012d83bc6f65eae3eb7b56818187
nicolajelitto <nicolasinanj@gmail.com>
2025-01-15 18:52:28 +0100

    Added arithmetic and and boolean

---
+++ arithmetic_op.py
@@ -0,0 +1,9 @@
+print(2+6)
+print(9-99)
+print(3*8)
+print(20/5)
+print(22/5)
+print(22//5) # always produces float result
+print(22%5)
+print(10%6)
+print(2**3)
---
+++ bool_operators.py
@@ -0,0 +1,11 @@
+print(not True)
+print(not False)
+print(not 7)
+
+print(True or False)
+print(True and False)
+print(2 or 3 or 4) # for or always first one (that is true) or last one if all are false
+print(False or 0 or 4 or True)
+print(1 and 2 and 3) # for and always last one
+print(1 and 0.0 and 4)
+print(False or 0 or None) #
